8ef0a4864d0a67e612505fe392084f05f89b0b66
Gregory Ross <gregross.dev@gmail.com>
2022-10-26 10:08:46 -0400

    mutate ref value with click event listener and utilize event modifier

--- 00-basics/src/App.vue
+++ 00-basics/src/App.vue
@@ -1,12 +1,37 @@
 <script setup>
 import { ref } from 'vue'
-const message = ref("Yo!!!!")
+
+const message = ref('Hello World!')
+
+function reverseMessage() {
+  // Access/mutate the value of a ref via
+  // its .value property.
+  message.value = message.value.split('').reverse().join('')
+}
+
+function notify() {
+  alert('navigation was prevented.')
+}
 </script>
 
 <template>
-<h1>{{ message }}</h1>
+
+  <h1>{{ message }}</h1>
+  <button @click="reverseMessage">Reverse Message</button>
+  <button @click="message += '!'">Append "!"</button>
+
+  <!--
+    Vue also provides modifiers for common tasks
+    such as e.preventDefault() and e.stopPropagation()
+  -->
+  <a href="https://vuejs.org" @click.prevent="notify">
+    A link with e.preventDefault()
+  </a>
 </template>
 
-<style scoped>
-
+<style>
+button, a {
+  display: block;
+  margin-bottom: 1em;
+}
 </style>
